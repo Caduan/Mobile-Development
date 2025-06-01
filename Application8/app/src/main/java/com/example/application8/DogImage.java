b6478637669be97145f2af44990893b072999164
nikitanikonov <loser.alanwolker2005@gmail.com>
2025-06-01 21:15:20 +0300

    Application 8

---
+++ Application8/app/src/main/java/com/example/application8/DogImage.java
@@ -0,0 +1,22 @@
+package com.example.application8;
+
+public class DogImage {
+	private String url;
+	private long fileSizeBytes;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public long getFileSizeBytes() {
+		return fileSizeBytes;
+	}
+
+	public void setFileSizeBytes(long fileSizeBytes) {
+		this.fileSizeBytes = fileSizeBytes;
+	}
+}
